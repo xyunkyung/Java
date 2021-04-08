d6fb3e445529374edb8a8ff152226d7519daa733
xyunkyung <lhkkkk@naver.com>
2021-04-08 12:23:24 +0900

    20210407HW

---
+++ PromotionExample.java
@@ -0,0 +1,25 @@
+
+public class PromotionExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		byte byteValue = 10;
+		int intValue = byteValue;
+		System.out.println(intValue);
+		
+		char charValue = '가';
+		intValue = charValue;
+		System.out.println("가의 유니코드 = " + intValue);
+		
+		intValue = 500;
+		long longValue = intValue;
+		System.out.println(longValue);
+		
+		intValue = 200;
+		double doubleValue = intValue;
+		System.out.println(doubleValue);
+		
+	}
+
+}
