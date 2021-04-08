5f78a985fbb439194f630c285f8399571285cbaa
xyunkyung <50695960+xyunkyung@users.noreply.github.com>
2021-04-08 15:28:28 +0900

    Add files via upload

---
+++ 20210407HW/CharExample.java
@@ -0,0 +1,23 @@
+
+public class CharExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		char c1 = 'A';
+		char c2 = 65;
+
+		char c4 = '°¡';
+		char c5 = 44032;
+
+		int unicode = c1;
+		
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c4);
+		System.out.println(c5);
+		System.out.println(unicode);
+		
+	}
+
+}
---
+++ 20210407HW/VariableExample.java
@@ -0,0 +1,14 @@
+
+public class VariableExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int value = 10;
+		int result = value + 10;
+		
+		System.out.println(result);
+		
+	}
+
+}
