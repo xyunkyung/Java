390033504af3e9b97088bf2b043f2862896e4534
xyunkyung <50695960+xyunkyung@users.noreply.github.com>
2021-04-08 15:25:27 +0900

    20210406

---
+++ src/0406_2.java
@@ -0,0 +1,16 @@
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		int i = 10;
+		int result = i + 1;
+		
+		System.out.println(result);
+		
+		int j;
+		j = 10;
+		System.out.println(j);
+
+	}
+
+}
