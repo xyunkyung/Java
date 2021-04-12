bd3b0df8cdb5604f5c107d77723998b39722225f
xyunkyung <50695960+xyunkyung@users.noreply.github.com>
2021-04-12 09:00:03 +0900

    Add files via upload

---
+++ 20210409HW/SwitchNoBreakCaseExample.java
@@ -0,0 +1,23 @@
+
+public class SwitchNoBreakCaseExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int time = (int)(Math.random() * 4) + 8;	// 8 ~ 11
+		System.out.println("[현재시간 : " + time + " 시]");
+		
+		switch(time) {
+		case 8:
+			System.out.println("출근합니다.");
+		case 9:
+			System.out.println("회의를 합니다.");
+		case 10:
+			System.out.println("업무를 봅니다.");
+		case 11:
+			System.out.println("외근을 나갑니다.");
+		}
+	}
+
+	// 따라서 break 문이 없으면 case 연달아 실행됨 
+}
