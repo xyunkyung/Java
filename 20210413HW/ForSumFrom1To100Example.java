c6893f13cbac8de5d3bfce50ee1f8c575c5ea1aa
xyunkyung <50695960+xyunkyung@users.noreply.github.com>
2021-04-14 09:36:31 +0900

    Add files via upload

---
+++ 20210413HW/ForSumFrom1To100Example.java
@@ -0,0 +1,16 @@
+
+public class ForSumFrom1To100Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int sum = 0;
+		
+		for(int i = 1; i <= 100; i++) {
+			sum += i;
+		}
+		
+		System.out.println("1 ~ 100 гу : " + sum);
+	}
+
+}
